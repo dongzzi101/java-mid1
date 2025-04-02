4877c62070d5d7d4bb5e58fc00588947eb2b7ca9
dongzzi101 <dongzzi101@gmail.com>
2025-04-03 00:37:26 +0900

    [equals() - 2. 구현] :
    equals() 메서드를 구현할 때 지켜야 하는 구칙
    - 반사성
    - 대칭성
    - 추이성
    - 인관성
    - null에 대한 비교

--- src/lang/object/equals/UserV2.java
+++ src/lang/object/equals/UserV2.java
@@ -1,5 +1,7 @@
 package lang.object.equals;
 
+import java.util.Objects;
+
 public class UserV2 {
 
     private String id;
@@ -8,9 +10,18 @@ public class UserV2 {
         this.id = id;
     }
 
+//    @Override
+//    public boolean equals(Object obj) {
+//        UserV2 user = (UserV2) obj;
+//        return id.equals(user.id);
+//    }
+
+
     @Override
-    public boolean equals(Object obj) {
-        UserV2 user = (UserV2) obj;
-        return id.equals(user.id);
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        UserV2 userV2 = (UserV2) o;
+        return Objects.equals(id, userV2.id);
     }
 }
+
