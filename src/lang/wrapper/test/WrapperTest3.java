b8662b83047f93cd3e5cccc560e628fbe4f21e84
dongzzi101 <dongzzi101@gmail.com>
2025-04-04 16:55:18 +0900

    [문제와 풀이1]

---
+++ src/lang/wrapper/test/WrapperTest3.java
@@ -0,0 +1,23 @@
+package lang.wrapper.test;
+
+public class WrapperTest3 {
+
+    public static void main(String[] args) {
+        String str = "100";
+
+        // String -> Integer
+        Integer integer1 = Integer.valueOf(str);
+
+        // Integer -> int
+        int intValue = integer1.intValue();
+
+        // int -> Integer
+        Integer integer2 = Integer.valueOf(intValue);
+
+        System.out.println("integer1 = " + integer1);
+        System.out.println("intvalue = " + intValue);
+        System.out.println("integer2 = " + integer2);
+
+
+    }
+}
